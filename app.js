11a1cfa145786d95bdeb61caa5cc6910d8f2141c
Dre_ <andre.garvin@envirostudies.org>
2017-04-20 21:00:32 +0000

    adding my script file foir the translate connection to Google translate API

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        msg: 'Hello, world'
+$(document).ready(function() {
+    
+    $('#translate').click(function() {
+        translate();
+    });
+});
+var API_key = 'AIzaSyCAg5StLQ3UHyaCdIdihjV8Od5sd8hGsDc';
+
+function translate( obj ) {
+    
+    var url_source = 'https://www.googleapis.com/language/translate/v2?q='+ obj.text +'&target=zh&key=' + API_key;
+    var url_translate = ' https://www.googleapis.com/language/translate/v2?q='+ obj.text +'&target=zh&source='
+    // ;obj.sourceLang +'&key=' + API_key;
+    
+    if ( obj.type === 'sourceLang' ) {
+        
+        return;
     }
-})
+    
+    
+}
+
+function detect( text ) {
+    var url = 'https://www.googleapis.com/language/translate/v2/detect?q=this+is+text&key=' + API_key; 
+}
