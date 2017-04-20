d180dde15f7de55a55e22e586ab2ffc5e71a4013
Dre_ <andre.garvin@envirostudies.org>
2017-04-20 22:19:27 +0000

    uploading the chnages to the script

--- translate.js
+++ translate.js
@@ -1,26 +1,44 @@
-$(document).ready(function() {
+// $(document).ready(function() {
     
-    $('#translate').click(function() {
-        translate();
-    });
-});
+//     $('#translate').click(function() {
+//         detect({
+//             type: 'transalte',
+//             text: 12321,
+//         });
+//     });
+// });
 
-var API_key = 'AIzaSyCAg5StLQ3UHyaCdIdihjV8Od5sd8hGsDc';
 
 function translate( obj ) {
     
-    var url_source = 'https://www.googleapis.com/language/translate/v2?q='+ obj.text +'&target=zh&key=' + API_key;
-    var url_translate = ' https://www.googleapis.com/language/translate/v2?q='+ obj.text +'&target=zh&source='
-    // ;obj.sourceLang +'&key=' + API_key;
-    
-    if ( obj.type === 'sourceLang' ) {
+    var API_key = 'AIzaSyCAg5StLQ3UHyaCdIdihjV8Od5sd8hGsDc';
+    var url = 'https://www.googleapis.com/language/translate/v2?q='+ obj.text +'&target='+ obj.language +'&key=' + API_key;
+
+    $.getJSON(url, function( resp ) {
         
-        return;
-    }
-    
-    
+        resp = resp.data.translations[0];
+        
+        obj.resp_text = resp.translatedText;
+        obj.detectLang = resp.detectedSourceLanguage;
+        
+        console.log( obj );
+    })
 }
 
-function detect( text ) {
-    var url = 'https://www.googleapis.com/language/translate/v2/detect?q=this+is+text&key=' + API_key; 
-}
+// function detect( obj ) {
+    
+//     var url = 'https://www.googleapis.com/language/translate/v2/detect?q='+ obj.text +'&key=' + API_key;
+    
+//     $.getJSON(url, function( resp ) {
+        
+//         resp = resp.data.detections[0][0];
+        
+//         if ( resp.language !== 'und' ) {
+//             obj.language = resp.language;
+            
+//             translate( obj );
+//             return;
+//         }
+//         console.log('Not a language');
+//     });
+// }
