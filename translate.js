b11bd9f7f88e0a7d8b12b9d922e3202353d5296b
Dre_ <andre.garvin@envirostudies.org>
2017-05-04 22:17:57 +0000

    created a two way data binding function called bindInput() and fixed the google translate API

--- translate.js
+++ translate.js
@@ -1,7 +1,10 @@
 function translate( obj ) {
     
-    var API_key = 'AIzaSyCAg5StLQ3UHyaCdIdihjV8Od5sd8hGsDc';
-    var url = 'https://www.googleapis.com/language/translate/v2?q='+ obj.text +'&target='+ obj.language +'&key=' + API_key;
+    var _0xeca7 = ["\x41\x49\x7A\x61\x53\x79\x44\x51\x33\x2D\x5F\x74\x50\x68\x52\x41\x75\x36\x62\x63\x6D\x39\x75\x5A\x73\x66\x49\x2D\x4F\x67\x59\x63\x61\x67\x76\x48\x55\x6D\x41"];
+    var API_KEY= _0xeca7[0];
+    
+    
+    var url = 'https://www.googleapis.com/language/translate/v2?q='+ obj.text.split(' ').join('+') +'&target='+ obj.lang +'&key=' + API_KEY;
 
     $.getJSON(url, function( resp ) {
         
@@ -14,42 +17,76 @@ function translate( obj ) {
     })
 }
 
+
+function bindInput( targetInput, value ) {
+    
+    
+    if ( value.length !== 0 ) {
+        
+        switch ( targetInput ) {
+            case 'input_1':
+                
+                $('#input_2').val( value );
+                break;
+            case 'input_2':
+            
+                $('#input_1').val( value );
+                break;
+            default:
+                break;
+        }
+    }
+    else {
+        switch ( targetInput ) {
+            case 'input_1':
+                
+                $('#input_2').val( value );
+                break;
+            case 'input_2':
+            
+                $('#input_1').val( value );
+                break;
+            default:
+                break;
+        }
+    }
+    
+}
+
+
 $(document).ready(function() {
+    
     $.getJSON('https://www.googleapis.com/language/translate/v2/languages?target=en&key=AIzaSyCAg5StLQ3UHyaCdIdihjV8Od5sd8hGsDc', function( resp ) {
         resp = resp.data.languages;
         
         for ( var i in resp ) {
-            $('.dropdown-menu').append('<li><a href="#">' + resp[i].name + '</a></li>');
+            
+            $('.dropdown-menu').append('<li id='+ resp[i].language +' class="lang"><a href="#">' + resp[i].name + '</a></li>');
         }
     });
     
-    $('#input_1').keydown(function(e) {
+    $('.Txt').keyup(function( e ) {
         
-        if ( $('#input_1').val().length === 0 ) {
-            $('#input_2').val('');
-            return;
+        if ( $('textarea').attr('id') === 'input_1' || 
+            $('textarea').attr('id') === 'input_2' ) {
+            
+            var input = $('textarea').attr('id');
+            
+            $('.Txt').keydown(function( e ) {
+        
+                if ( e.which === 8 && $('#'+input).val().length === 1 ) {
+            
+                    bindInput( input, '' );
+                }
+            });
+            
+            bindInput( input, $('#'+$('textarea').attr('id')).val() );
         }
-        
-        $('#input_2').val( $('#input_1').val() );
     });
     
+    $('.lang').click(function() {
+        
+        console.log('bdbsja');
+    });
     
 });
-
-// function detect( obj ) {
-    
-//     var url = 'https://www.googleapis.com/language/translate/v2/detect?q='+ obj.text +'&key=' + API_key;
-    
-//     $.getJSON(url, function( resp ) {
-        
-//         resp = resp.data.detections[0][0];
-        
-//         if ( resp.language !== 'und' ) {
-//             obj.language = resp.language;
-            
-//             translate( obj );
-//             return;
-//         }
-//         console.log('Not a language');
-//     });
-// }
